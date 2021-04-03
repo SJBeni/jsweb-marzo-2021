c83f2eb807e3462cebfb47bac62d3aa1b4f4267c
Sergio <Sergio@192.168.0.13>
2021-04-03 00:59:15 -0300

    Clase 3 ( Interfaces + Exceptions + Ejemplos)

--- app-web/src/main/java/ar/com/educacionit/exceptions/checked/DivisionPorCeroException.java
+++ app-web/src/main/java/ar/com/educacionit/exceptions/checked/DivisionPorCeroException.java
@@ -5,6 +5,7 @@ public class DivisionPorCeroException extends Exception {
 	public DivisionPorCeroException(String message, Throwable cause) {
 		super(message, cause);
 		//constructor de la superclase
+		//otro comentario
 		// TODO Auto-generated constructor stub
 	}
 
